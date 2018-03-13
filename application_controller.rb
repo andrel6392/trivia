36104cb7bc51c688a866b0694771c890996245b6
andrel6392 <andrel6392@hstat.org>
2018-03-13 04:54:04 +0000

    arrays instead of hashes to pass values

--- application_controller.rb
+++ application_controller.rb
@@ -1,5 +1,6 @@
 require 'dotenv/load'
 require 'bundler'
+require 'pp'
 Bundler.require
 
 require_relative 'models/data-sorter.rb'
@@ -12,19 +13,25 @@ class ApplicationController < Sinatra::Base
 
 
   post '/questions' do 
-    puts params
     @user_topic = params["trivia-topic"]
     @user_num = params["trivia-qnumber"]
     @user_hash = get_trivia_questions(@user_topic, @user_num)
-    @user_hash_keys = @user_hash.keys
-    @user_hash_keys.shift
+    @quiz_questions = @user_hash.keys
+    @quiz_questions.shift 
+    @quiz_options = @user_hash.values #2 dimensional array of options for each question
+    @quiz_answers = @quiz_options.shift #answers for questions
+    puts "Questions:"
+    pp @quiz_questions
+    puts "Options:" 
+    pp @quiz_options
+    puts "Answers:"
+    pp @quiz_answers
     erb :questions
   end 
   
-  post '/results' do 
-    @user_answer = params
-    puts @user_hash = params[:user_hash]
-    puts @user_hash_keys= params[:user_hash_keys]
+  post '/results' do
+    puts params
+    @questions = params["quiz_questions"]
     erb :results
     
   end 
